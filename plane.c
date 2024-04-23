8a413ae175c008f57e380715a801e569c268c98b
vidhimht <134291969+vidhimht@users.noreply.github.com>
2024-04-23 17:51:06 +0530

    Update plane.c

--- plane.c
+++ plane.c
@@ -1,35 +1,50 @@
-// updated one
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h> // is this needed
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <pthread.h>
-#include <semaphore.h>
 
-#define MSG_QUEUE_KEY 'k'
+#define MAX_PASSENGERS 10
+#define MAX_CARGO_ITEMS 100
+#define MAX_CREW_MEMBERS 7
+#define MAX_CREW_CARGO 2
+#define MAX_WEIGHT_LUGGAGE 25
+#define MAX_WEIGHT_PASSENGER 100
+#define AVG_WEIGHT_CREW 75
+#define BOARDING_TIME 3
+#define DEBOARDING_TIME 3
+#define JOURNEY_TIME 5
+#define MSG_QUEUE_KEY 'atc_message_queue'
 
 struct PlaneInfo
 {
     int planeID;
     int planeType;
+
     int numOccupiedSeats;   // or total number of cargo
     int totalLuggageWeight; // or total cargo weight
+
     int totalPassengerWeight;
     int totalCrewWeight;
+
     int departureAirport;
     int arrivalAirport;
+
     int totalPlaneWeight;
 };
 
 struct NotificationMessage
 {
-    int flag;
-    int kill_status;
+    int flag; //0 - plane, 1 - airport
+    int kill_status;      // 0: don't kill, 1: self kill for plane/airport, 2: force kill for plane
     int completionStatus; // This field can indicate the status of the deboarding/unloading process
+    // 1 : departure plane started boarding
+    // 2 : departure plane finished boarding, now in flight
+    // 4 : departure plane finished deboarding
 };
 
 struct msg_buffer
@@ -39,251 +54,274 @@ struct msg_buffer
     struct NotificationMessage notification; // the notification
 };
 
-struct Runway
+struct PassengerInfo
 {
-    int id;
-    int loadCapacity;
-    int isAvailable;
-    sem_t semaphore;
-    pthread_mutex_t mutex;
+    int luggageWeight;
+    int bodyWeight;
 };
-
-struct DepartureArgs
-{
-    struct msg_buffer message;
-    struct Runway *runways;
-    int numRunways;
-};
-
-struct ArrivalArgs
-{
-    struct msg_buffer message;
-    struct Runway *runways;
-    int numRunways;
-};
-
-void *handleDeparture(void *arg)
-{
-    struct DepartureArgs *departure = (struct DepartureArgs *)arg;
-    int selectedRunway = 0;
-    int planeID = departure->message.plane.planeID;
-    int airportNumber = departure->message.plane.departureAirport;
-    printf("Total weight of the plane is: %d", departure->message.plane.totalPlaneWeight);
-
-    for (int i = 0; i <= departure->numRunways; i++)
-    {
-        
-        if (departure->message.plane.totalPlaneWeight <= departure->runways[i].loadCapacity)
-        {
-            if (departure->runways[i].loadCapacity < departure->runways[selectedRunway].loadCapacity)
-            {
-                selectedRunway = i;
-
-            }
-        }
-    }
-    printf("Best runway for planeID %d is %d\n", planeID, selectedRunway);
-
-    sem_wait(&departure->runways[selectedRunway].semaphore);
-    printf("Waiting state...\n");
-    sleep(3); // using the runway
-    sem_post(&departure->runways[selectedRunway].semaphore);
-    printf("Plane %d has completed boarding/loading and taken off from Runway No. %d of Airport No. %d.\n", planeID, selectedRunway, airportNumber);
-    // simulate takeoff
-    sleep(2);
-
-
-    return NULL;
-}
-
-void *handleArrival(void *arg)
-{
-    printf("arrival airport is handling arrival\n");
-    struct ArrivalArgs *arrival = (struct ArrivalArgs *)arg;
-    int selectedRunway = 0;
-    int planeID = arrival->message.plane.planeID;
-    int airportNumber = arrival->message.plane.arrivalAirport;
-
-    // finding best fit runway
-    for (int i = 0; i <= arrival->numRunways; i++)
-    {
-        if (arrival->message.plane.totalPlaneWeight <= arrival->runways[i].loadCapacity)
-        {
-            if (arrival->runways[i].loadCapacity < arrival->runways[selectedRunway].loadCapacity)
-            {
-                selectedRunway = i;
-            
-            }
-        }
-    }
-    printf("best fit is runway %d\n", selectedRunway);
-    sem_wait(&arrival->runways[selectedRunway].semaphore);
-    sleep(5); // plane in-flight
-    sleep(3); // using the runway
-    sem_post(&arrival->runways[selectedRunway].semaphore);
-
-    printf("Plane %d has landed on Runway No. %d of Airport No. %d  and has completed deboarding/unloading.\n", planeID, selectedRunway, airportNumber);
-
-    return NULL;
-}
 
 int main()
 {
-    int airportNumber;
-    int runwaysCount;
+    // int planes[MAX_PASSENGERS][6]; // 2D array to store plane information (shrishti:not needed)
+
+    // generate a mapping of which index of plane array stores wht info
+
+    // 0 - planeID
+    // 1 - planeType
+
+    // 2 - arrivalAirport
+    // 3 - departureAirport
+
+    // 4 - totalPlaneWeight
+    // 5 number of passengers (only for passenger planes)
+
+    int planeID;
+    int planeType;
+    int numOccupiedSeats;
+    int totalLuggageWeight = 0;
+    int totalPassengerWeight = 0;
+    int totalCrewWeight = 0;
+    int departureAirport;
+    int arrivalAirport;
+    int totalPlaneWeight;
 
     key_t key;
     struct msg_buffer message;
 
-    printf("Enter Airport Number: ");
-    scanf("%d", &airportNumber);
+    printf("Enter Plane ID: ");
+    scanf("%d", &planeID);
 
-    // Validate the airport number
-    if (airportNumber < 1 || airportNumber > 10)
-    {
-        // printf("Invalid airport number. Please enter a valid airport number between 1 and 10.\n");
-        return 1;
-    }
-
-    // Input number of runways
-    printf("Enter number of Runways: ");
-    scanf("%d", &runwaysCount);
-
-    if (runwaysCount < 1 || runwaysCount > 10)
-    {
-        printf("Invalid number of runways. Please enter a valid number between 1 and 10.\n");
-        return 1;
-    }
-
-    // Array to hold the capacity of each runway
-    struct Runway runways[runwaysCount + 1];
-
-    // Backup runway always at index 0
-    runways[0].loadCapacity = 15000;
-    runways[0].isAvailable = 1;
-    printf("runway count itna hai:%d\n", runwaysCount);
-    // Initializing rest of the runways
-    printf("Enter loadCapacity of Runways (give as a space separated list in a single line):");
-    int temp[runwaysCount];
-    for (int i = 0; i < runwaysCount; i++)
-    {
-        scanf("%d", &temp[i]);
-    }
-    for (int i = 0; i < runwaysCount + 1; i++)
-    {
-        if (i != runwaysCount)
-        {
-            if (temp[i] < 1000 || temp[i] > 12000)
-            {
-                printf("Invalid load capacity. Please enter a valid capacity between 1000 and 12000.\n");
-                return 1;
-            }
-            runways[i + 1].loadCapacity = temp[i];
-            runways[i + 1].isAvailable = 1;
-        }
-        if (sem_init(&runways[i].semaphore, 0, 0) != 0)
-        {
-            printf("Error in initializing semaphore for runway %d\n", i);
-            return 1;
-        }
-        printf("runway %d has limit %d\n", i, runways[i].loadCapacity);
-    }
-
-    // Logic to receive message from ATC
-    key = ftok("plane.c", 'x'); // Change path to atc.c when that file is made for uniformity.
+    // ftok to generate unique key,planeID as unique key
+    key = ftok("plane.c", 'x'); // change path to atc.c when that file is made for uniformity.
     if (key == -1)
     {
-        printf("Error in creating unique key\n");
+        printf("error in creating unique key\n");
         exit(1);
     }
 
-    // msgget creates a message queue and returns identifier
+    // msgget creates a message queue
+    // and returns identifier
     const int msgid = msgget(key, 0666 | IPC_CREAT);
     if (msgid == -1)
     {
-        printf("Error in creating message queue\n");
+        printf("error in creating message queue\n");
         exit(1);
     }
 
-    int airportactive = 1;
-    while (airportactive == 1)
+    // Validate the plane ID
+    if (planeID < 1 || planeID > 10)
     {
-        // Receives the message from the message queue with the flight details
-        while(msgrcv(msgid, &message, sizeof(message), airportNumber + 10, 0) == -1)
-        {
-        }
- printf("msg received with type: %d and status: %d\n", message.msg_type, message.notification.completionStatus);
-        // if (msgrcv(msgid, &message, sizeof(message), airportNumber+10, 0) == -1)
-        // {
-        //     printf("Error in receiving message\n");
-        //     exit(1);
-        // }
-       
-        printf("plane is saying depart from %d & arrive to %d\n", message.plane.departureAirport, message.plane.arrivalAirport);
-        // Plane wants to depart from this airport
-        if (message.notification.flag==3 && message.notification.completionStatus == 1)
-        {
-            struct DepartureArgs departure;
-            departure.message = message;
-            departure.runways = runways;
-            departure.numRunways = runwaysCount;
+        fprintf(stderr, "Invalid plane ID. Please enter a valid ID between 1 and 10.\n");
+        return 1;
+    }
+    // Request the type of plane from the user
+    printf("Enter Type of Plane: ");
+    scanf("%d", &planeType);
 
-            pthread_t departure_thread;
-            if (pthread_create(&departure_thread, NULL, handleDeparture, (void *)&departure) != 0)
+    // planes[planeID - 1][0] = planeID;  not needed
+    // planes[planeID - 1][1] = planeType;  mot needed
+
+    if (planeType == 1)
+    {
+        // printf("This is a passenger plane.\n");
+        //  Request the number of occupied seats for the passenger plane
+        printf("Enter Number of Occupied Seats : ");
+        scanf("%d", &numOccupiedSeats);
+
+        // Validate the number of occupied seats //idt we need still
+        if (numOccupiedSeats < 1 || numOccupiedSeats > 10)
+        {
+            fprintf(stderr, "Invalid number of occupied seats. Please enter a value between 1 and 10.\n");
+            return 1;
+        }
+
+        // Store the number of occupied seats in the planes array
+        // planes[planeID - 1][5] = numOccupiedSeats; // 5 is the index for number of passengers
+
+        // Create an array to store passenger information
+        struct PassengerInfo passengerInfo[MAX_PASSENGERS];
+
+        // Create child processes and pipes for each passenger
+        int passengers = numOccupiedSeats;
+        int passengerCount;
+        pid_t passengerPIDs[MAX_PASSENGERS]; // Array to store passenger PIDs
+        int passengerPipes[MAX_PASSENGERS][2];
+
+        for (passengerCount = 0; passengerCount < passengers; passengerCount++)
+        {
+            if (pipe(passengerPipes[passengerCount]) < 0)
             {
-                printf("Error in creating thread for plane %d\n", message.plane.planeID);
+                fprintf(stderr, "Error creating pipe for passenger %d\n", passengerCount + 1);
                 return 1;
             }
 
-            
-            // send arrival message to arrival airport
-         //   message.msg_type = airportNumber + 20;
-            message.msg_type = 100;
-            message.notification.flag = 1;
-            message.notification.completionStatus = 2;
+            pid_t pid = fork();
 
-            if (msgsnd(msgid, &message, sizeof(message), 0) == -1)
+            if (pid < 0)
             {
-                printf("error in sending arrival message\n");
-                exit(1);
-            }
-            printf("arrival inbound message sent to ATC\n");
-            
-            pthread_join(departure_thread, NULL);
-        }
-        // Plane wants to arrive to this airport
-        else if (message.notification.flag==4 && message.notification.completionStatus == 2)
-        {
-            printf("ATC told me flight inbound\n");
-            struct ArrivalArgs arrival;
-            arrival.message = message;
-            arrival.runways = runways;
-            arrival.numRunways = runwaysCount;
-
-            pthread_t arrival_thread;
-            if (pthread_create(&arrival_thread, NULL, handleArrival, (void *)&arrival) != 0)
-            {
-                printf("Error in creating thread for plane %d\n", message.plane.planeID);
+                // Fork failed
+                fprintf(stderr, "Fork failed for passenger %d\n", passengerCount + 1);
                 return 1;
             }
-
-            pthread_join(arrival_thread, NULL);
-            // send arrival message to ATC
-            message.msg_type = airportNumber + 20;
-            message.notification.completionStatus = 4;
-            if (msgsnd(msgid, &message, sizeof(message), 0) == -1)
+            else if (pid == 0)
             {
-                printf("error in sending arrival complete message\n");
-                exit(1);
+                // Child process (passenger)
+                printf("Enter Weight of Your Luggage: ");
+                scanf("%d", &passengerInfo[passengerCount].luggageWeight);
+
+                // Validate the luggage weight
+                if (passengerInfo[passengerCount].luggageWeight < 0 || passengerInfo[passengerCount].luggageWeight > 25)
+                {
+                    fprintf(stderr, "Invalid luggage weight. Please enter a value between 0 and 25.\n");
+                    return 1;
+                }
+
+                printf("Enter Your Body Weight: ");
+                scanf("%d", &passengerInfo[passengerCount].bodyWeight);
+
+                // Validate the body weight
+                if (passengerInfo[passengerCount].bodyWeight < 10 || passengerInfo[passengerCount].bodyWeight > 100)
+                {
+                    fprintf(stderr, "Invalid body weight. Please enter a value between 10 and 100.\n");
+                    return 1;
+                }
+
+                // Communicate the luggage weight and body weight to the plane process through the pipe
+                close(passengerPipes[passengerCount][0]); // Close the read end of the pipe
+                write(passengerPipes[passengerCount][1], &passengerInfo[passengerCount], sizeof(struct PassengerInfo));
+                close(passengerPipes[passengerCount][1]); // Close the write end of the pipe
+
+                // for checking concurrency
+                printf("Passenger %d process with PID %d is ready\n", passengerCount + 1, getpid());
+                exit(0);
             }
+            wait(NULL);
+            //  else
+            //  {
+            //      // Parent process
+            //      close(passengerPipes[passengerCount][1]); // Close the write end of the pipe
+            //  }
         }
 
-        else
+        // Access and utilize passenger information in the plane process
+        for (int i = 0; i < passengers; i++)
         {
-            printf("This message is prolly not for this airport.\n");
+            struct PassengerInfo receivedInfo;
+            read(passengerPipes[i][0], &receivedInfo, sizeof(struct PassengerInfo));
+            close(passengerPipes[i][0]);
+            totalLuggageWeight += receivedInfo.luggageWeight;
+            totalPassengerWeight += receivedInfo.bodyWeight;
+            // printf("Passenger %d - Luggage Weight: %d, Body Weight: %d\n", i + 1, receivedInfo.luggageWeight, receivedInfo.bodyWeight);
         }
 
-        // check if airport needs to close
+        // Calculate the total crew weight
+        int totalCrewMembers = 7; // 2 pilots + 5 flight attendants
+        int averageCrewWeight = 75;
+        totalCrewWeight = totalCrewMembers * averageCrewWeight;
+
+        // Calculate the total weight of the plane
+         totalPlaneWeight = totalLuggageWeight + totalPassengerWeight + totalCrewWeight;
+        // planes[planeID - 1][4] = totalPlaneWeight; not needed
+    }
+    else
+    {
+        // Cargo plane
+        printf("Enter Number of Cargo Items: ");
+        scanf("%d", &numOccupiedSeats); // using the same variable as for passengers
+
+        if (numOccupiedSeats < 1 || numOccupiedSeats > 100)
+        {
+            fprintf(stderr, "Invalid number of cargo items. Please enter a value between 1 and 100.\n");
+            return 1;
+        }
+
+        printf("Enter Average Weight of Cargo Items: ");
+        scanf("%d", &totalPassengerWeight); // this is actually the average weight
+        // using the same variable as passengers
+
+        if (totalPassengerWeight < 1 || totalPassengerWeight > 100)
+        {
+            fprintf(stderr, "Invalid average weight of cargo items. Please enter a value between 1 and 100.\n");
+            return 1;
+        }
+        totalPassengerWeight = numOccupiedSeats * totalPassengerWeight;
+        // Calculate total crew weight
+        int totalCrewMembers = 2; // 2 pilots for cargo plane
+        int averageCrewWeight = 75;
+        totalCrewWeight = totalCrewMembers * averageCrewWeight;
+
+        // Calculate total plane weight
+        totalPlaneWeight = totalPassengerWeight + totalCrewWeight;
+
+        // planes[planeID - 1][4] = totalPlaneWeight; not needed
+    }
+    printf("Enter Departure Airport: ");
+    scanf("%d", &departureAirport);
+
+    // planes[planeID - 1][3] = departureAirport; not needed
+
+    printf("Enter Arrival Airport: ");
+    scanf("%d", &arrivalAirport);
+
+    // planes[planeID - 1][2] = arrivalAirport; not needed
+
+    // Send plane information through message queue
+    message.msg_type = 100;
+    message.notification.flag = 0;
+    message.plane.planeID = planeID;
+    message.plane.planeType = planeType;
+    message.plane.numOccupiedSeats = numOccupiedSeats;
+    message.plane.totalLuggageWeight = totalLuggageWeight;
+    message.plane.totalPassengerWeight = totalPassengerWeight;
+    message.plane.totalCrewWeight = totalCrewWeight;
+    message.plane.departureAirport = departureAirport;
+    message.plane.arrivalAirport = arrivalAirport;
+    message.plane.totalPlaneWeight = totalPlaneWeight;
+    message.notification.kill_status = 0;
+    message.notification.completionStatus = 1;
+    // msgsnd to send message
+    if (msgsnd(msgid, &message, sizeof(message), 0) == -1)
+    {
+        printf("error in sending message\n");
+        exit(1);
+    }
+    printf("plane weighs: %d\n", message.plane.totalPlaneWeight);
+    printf("departure message sent to ATC\n");
+    
+    
+    /*if (msgrcv(msgid, &message, sizeof(message), planeID+30, 0))
+    {
+        if (message.notification.kill_status == 2)
+        {
+            printf("Departure not possible");
+            return 0;
+        }
+    }*/
+    // Simulate the boarding/loading process
+ //   sleep(BOARDING_TIME); // Boarding/loading process
+
+    // Simulate the plane journey duration
+ //   sleep(JOURNEY_TIME); // Journey duration
+    //********************************
+    //********************************
+    // CHANGE THIS BACK TO 30 IN DEFINE
+    //********************************
+    //********************************
+
+    // Receive notification from the plane process
+ //   sleep(DEBOARDING_TIME); // deboarding/unloading process
+
+    while(msgrcv(msgid, &message, sizeof(message), planeID+30, 0) == -1){};
+
+    if (message.notification.kill_status == 2)
+    {
+        printf("Departure not possible");
+        return 0;
+    }
+    if (message.notification.kill_status == 1)
+    {
+        printf("Plane %d has successfully traveled from Airport %d to Airport %d!\n", planeID, departureAirport, arrivalAirport);
+        return 0;
     }
 
     return 0;
